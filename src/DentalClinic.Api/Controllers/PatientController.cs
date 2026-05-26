f6504121564af99ff4d06db584340876004e69f7
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-05-25 23:32:05 -0600

    Creacion PatientController

---
+++ src/DentalClinic.Api/Controllers/PatientController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DentalClinic.Api.Mappers;
+using DentalClinic.Api.Models.Requests;
+using DentalClinic.Exceptions;
+using DentalClinic.Facade;
+
+namespace DentalClinic.Api.Controllers;
+
+[ApiController]
+[Route("api/patients")]
+public class PatientController : ControllerBase
+{
+    private readonly IPatientFacade patientFacade;
+
+    public PatientController(IPatientFacade patientFacade)
+    {
+        this.patientFacade = patientFacade;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPatients()
+    {
+        var patients = await patientFacade.GetAllAsync();
+
+        var models = PatientMapper.ToModel(patients);
+
+        return Ok(models);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPatient(int patient_id)
+    {
+        try
+        {
+            var patient = await patientFacade.GetByIdAsync(patient_id);
+            
+            var model = PatientMapper.ToModel(patient);
+
+            return Ok(model);
+        }
+        catch (ResourceNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPatient([FromBody] CreatePatientRequestModel patient)
+    {
+        var dto = PatientMapper.ToDto(patient);
+        
+        var addedPatient = await patientFacade.AddAsync(dto);
+
+        var model = PatientMapper.ToModel(addedPatient);
+        
+        return CreatedAtAction(nameof(GetPatient), new {id = model.patient_id}, model);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePatient(int id)
+    {
+        try
+        {
+            await patientFacade.DeleteAsync(id);
+
+            return Ok();
+        }
+        catch (ResourceNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+}
